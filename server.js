9a0ed4a1905dc581b8dff87c401e81cb0176d759
Matthias Hogerheijde <matthias.hogerheijde@goabout.com>
2015-03-18 11:13:26 +0100

    Add check for Authorization header

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var renderHtml = function(url, cb) {
 
     page.settings.loadImages = false; //Browser will load images by itself so there is no need for phantom to load them
     page.settings.localToRemoteUrlAccessEnabled = true;
-    page.onCallback = function() {
 
+    page.onCallback = function() {
         //Javascript is removed so it won't affect the page after loading
         //Comment that part if you don't want JS scripts to be removed from the page
         page.evaluate(function() {
@@ -21,13 +21,12 @@ var renderHtml = function(url, cb) {
           };
         });
         //End
-
         cb(page.content);
         page.close();
     };
 
     page.onConsoleMessage = function(msg, lineNum, sourceId) {
-       console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+       console.debug('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
     }; //For service purposes only
 
     page.onInitialized = function() {
@@ -37,12 +36,14 @@ var renderHtml = function(url, cb) {
             }, 10000); //If no callPhantom script was called on the page, then wait 10secs and load page anyway.
         });
     };
+
+    console.debug("Opening page");
     page.open(url);
 
     page.onError = function (msg, trace) {
-        console.log(msg);
+        console.error(msg);
         trace.forEach(function(item) {
-            console.log('  ', item.file, ':', item.line);
+            console.error('  ', item.file, ':', item.line);
         });
     };
 };
@@ -104,16 +105,39 @@ var config = configure()
 var server = require('webserver').create();
 var urlPrefix = config['url'];
 var port = config['port'] || system.env.PORT || 8082;
-var userAgent = config['user'];
+var key = config['key'];
+var userAgent = config['user_agent'];
 
 
 server.listen(port, function (request, response) {
     var route = urlModule.parse(request.url).pathname;
     var url = request.headers['X-Download-From'] || urlPrefix; //Use 'Download-from' header if present, otherwise use default one
 
+
+    if (key) {
+        if (!request.headers['Authorization']) {
+            console.warn("Authorization needed; no Authorization headers found");
+            response.statusCode = 401;
+            response.write(fs.read('401.html'));
+            response.close();
+            return;
+        } else if (request.headers['Authorization'] !== "Bearer " + key) {
+            console.warn("Access denied; invalid Authorization header");
+            response.headers['User-Agent'] = userAgent;
+            response.statusCode = 403;
+            response.write(fs.read('403.html'));
+            response.close();
+            return;
+        }
+    }
+
+
+
     url = url + route; //Then add route path
 
     renderHtml(url, function(html) {
+        console.debug("Sending prerendered html to requester")
+
         response.headers['User-Agent'] = userAgent;
         response.statusCode = 200;
         response.write(html);
@@ -121,6 +145,8 @@ server.listen(port, function (request, response) {
     });
 });
 
-console.info('Listening on ' + port + '...');
-console.info('Default url  ' + urlPrefix + '...');
+console.info('Listening on   ' + port);
+console.info('Default url    ' + urlPrefix);
+console.info('Identifying as ' + userAgent);
+
 console.info('Press Ctrl+C to stop.');
